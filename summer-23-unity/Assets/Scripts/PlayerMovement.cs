70e61b834d9aa23d8d066605f7149fdeca28d8ff
Andrew Larson <48035438+alarson892@users.noreply.github.com>
2023-06-12 20:23:47 -0500

    Move speed + level edit
    Increased movement speed; added some platforms

--- summer-23-unity/Assets/Scripts/PlayerMovement.cs
+++ summer-23-unity/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {  
     public CharacterController controller;
     public Vector3 playerVelocity;
-    //private bool groundedPlayer;
+    public bool groundedPlayer;
     public float playerSpeed = 2.0f;
-    //private float jumpHeight = 1.0f;
-    //private float gravityValue = -9.81f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
 
     void Start()
     {
@@ -18,11 +18,11 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        //groundedPlayer = controller.isGrounded;
-        //if (groundedPlayer && playerVelocity.y < 0)
-        //{
-        //    playerVelocity.y = 0f;
-        //}
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+           playerVelocity.y = 0f;
+        }
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         controller.Move(move * Time.deltaTime * playerSpeed);
@@ -33,12 +33,12 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Changes the height position of the player..
-        // if (Input.GetButtonDown("Jump") && groundedPlayer)
-        // {
-        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        // }
+        if (Input.GetButtonDown("Jump") && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
 
-        // playerVelocity.y += gravityValue * Time.deltaTime;
-        //controller.Move(playerVelocity * Time.deltaTime);
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
     }
 }
